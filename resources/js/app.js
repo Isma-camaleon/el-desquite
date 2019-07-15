0d908521a6312514f4d23442b84c0a71d2dacb3e
Isma <ismael.c.26a@gmail.com>
2019-07-15 00:04:45 -0400

    Initial project

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+require('./bootstrap');
+require('admin-lte/dist/js/adminlte.beta.min.js');
+
+window.Vue = require('vue');
+
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+
+const app = new Vue({
+    el: '#app',
+});
